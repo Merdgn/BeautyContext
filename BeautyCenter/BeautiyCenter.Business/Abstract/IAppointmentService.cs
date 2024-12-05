36b63111aaa868a261226131258e21b66064f7e4
MERDGN <e.mirayerdgn40@gmail.com>
2024-12-05 20:43:13 +0300

    güzellik merkezi uygulaması
    ASP.NETCORE MVC ile yazılmıştır

---
+++ BeautyCenter/BeautiyCenter.Business/Abstract/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using BeautiyCenter.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautiyCenter.Business.Abstract
+{
+    public interface IAppointmentService:IGenericService<Appointment>
+    {
+    }
+}
