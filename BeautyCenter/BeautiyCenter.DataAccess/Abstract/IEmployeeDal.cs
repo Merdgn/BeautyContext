b5131125d55f54477a9b4296a56e90e270266353
MERDGN <e.mirayerdgn40@gmail.com>
2024-12-12 18:51:46 +0300

    BeautiyCenter
    Asp.Netcore Mvc 6 ile yazılmıştır

---
+++ BeautyCenter/BeautiyCenter.DataAccess/Abstract/IEmployeeDal.cs
@@ -0,0 +1,13 @@
+﻿using BeautiyCenter.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautiyCenter.DataAccess.Abstract
+{
+    public interface IEmployeeDal : IGenericDal<Employee>
+    {
+    }
+}
