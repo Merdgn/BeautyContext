a4a8805edb830962c20f1428575697aecce5ece3
MERDGN <e.mirayerdgn40@gmail.com>
2024-12-19 13:17:57 +0300

    beauty center
    AspNetCore Mvc ile yazılmıştır

---
+++ BeautyCenter/BeautyCenter/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautyCenter.Presentation.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        [Required(ErrorMessage = "Müşteri Adı girilmesi zorunludur.")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Müşteri İletişim bilgisi girilmesi zorunludur.")]
+        public string CustomerContact { get; set; }
+
+        [Required(ErrorMessage = "Hizmet seçimi zorunludur.")]
+        public int ServiceId { get; set; }
+
+        [Required(ErrorMessage = "Randevu tarihi seçimi zorunludur.")]
+        public DateTime AppointmentDate { get; set; }
+    }
+}
